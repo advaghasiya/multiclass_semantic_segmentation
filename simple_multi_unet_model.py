4940803f48859c5e9d52120284793a3397ba0b4d
Ankit <ankit.d.vaghasiya@gmail.com>
2024-03-27 17:43:10 +0100

    training is incomplete

--- simple_multi_unet_model.py
+++ simple_multi_unet_model.py
@@ -11,10 +11,11 @@ from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, concatenate,
 from keras import backend as K
 
 def jacard_coef(y_true, y_pred):
-    y_true_f = K.flatten(y_true)
-    y_pred_f = K.flatten(y_pred)
+    y_true_f = K.reshape(y_true, (-1,))
+    y_pred_f = K.reshape(y_pred, (-1,))
     intersection = K.sum(y_true_f * y_pred_f)
     return (intersection + 1.0) / (K.sum(y_true_f) + K.sum(y_pred_f) - intersection + 1.0)
+
 
 
 
